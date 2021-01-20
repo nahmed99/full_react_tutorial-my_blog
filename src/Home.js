8383c52ed77eb886483f92210af88415bf8cd8bc
nahmed99 <ahmed_nawaz@hotmail.com>
2021-01-20 22:12:16 +0000

    captured event in two different ways for button click in jsx

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,11 @@
 const Home = () => {
 
-   const handleClick = () => {
-      console.log("Kidda Yaar?");
+   const handleClick = (ev) => {
+      console.log("Kidda Yaar?", ev);
    }
 
-   const handleClickAgain = (name) => {
-      console.log("Kidda " + name);
+   const handleClickAgain = (name, ev) => {
+      console.log("Kidda " + name, ev.target);
    }
 
 
@@ -14,8 +14,8 @@ const Home = () => {
          <h2>Homepage</h2>
          {/* If the included the brackets after the function name below, it would be invoked automatically - without even waiting for the user to click it. Therefore we only provide a reference to the function name */}
          <button onClick={ handleClick }>Click It!</button>
-         <button onClick={ () => {
-            handleClickAgain('Yaara') // This is the way to pass arguments to a called function from here (and to prevent it being invoked automatically)
+         <button onClick={ (event) => {
+            handleClickAgain('Yaara', event) // This is the way to pass arguments to a called function from here (and to prevent it being invoked automatically). "event" can be passed into this function as a the first OR the second argument
          } }>Click It Again!</button>
       </div>
    );
