f5f64661d892429c67a78fb053a216f8cacdb43a
nahmed99 <ahmed_nawaz@hotmail.com>
2021-01-21 16:31:40 +0000

    Added nonReactiveName and tested. Then commented it out and added reactiveName and tested it - to show the webpage being re-rendered in the latter case, but not in the former

--- src/Home.js
+++ src/Home.js
@@ -1,22 +1,43 @@
+import { useState } from 'react';
+
 const Home = () => {
 
-   const handleClick = (ev) => {
-      console.log("Kidda Yaar?", ev);
-   }
+   // let nonReactivename = "Oi! Bhai Saab!"; // This variable is not REACTive! You need to put it into a useState for it to become REACTive!
+   // const handleClick = () => {
+   //    nonReactivename = "Ney Yaar";
+   //    console.log(nonReactivename);
+   // }
 
-   const handleClickAgain = (name, ev) => {
-      console.log("Kidda " + name, ev.target);
+   const [reactiveName, setReactiveName] = useState("Oi! Bhai Saab!");
+   const handleClick = () => {
+      setReactiveName("Ney Yaar");
+      console.log(reactiveName);
    }
+   
+   // const handleClick = (ev) => {
+   //    console.log("Kidda Yaar?", ev);
+   // }
+   
+   // const handleClickAgain = (name, ev) => {
+   //    console.log("Kidda " + name, ev.target);
+   // }
+
 
 
    return (
       <div className="home">
          <h2>Homepage</h2>
+
+         {/* <p>{ nonReactivename }</p> */}
+         <p>{ reactiveName }</p>
+
          {/* If the included the brackets after the function name below, it would be invoked automatically - without even waiting for the user to click it. Therefore we only provide a reference to the function name */}
          <button onClick={ handleClick }>Click It!</button>
-         <button onClick={ (event) => {
+         
+         {/* <button onClick={ (event) => {
             handleClickAgain('Yaara', event) // This is the way to pass arguments to a called function from here (and to prevent it being invoked automatically). "event" can be passed into this function as a the first OR the second argument
-         } }>Click It Again!</button>
+         } }>Click It Again!</button> */}
+
       </div>
    );
 }
