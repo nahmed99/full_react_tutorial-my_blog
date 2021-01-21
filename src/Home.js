b93920e7d38e55c03dc1f3874230fb3cfb96dd3e
nahmed99 <ahmed_nawaz@hotmail.com>
2021-01-21 18:20:22 +0000

    blog code started...

--- src/Home.js
+++ src/Home.js
@@ -2,42 +2,22 @@ import { useState } from 'react';
 
 const Home = () => {
 
-   // let nonReactivename = "Oi! Bhai Saab!"; // This variable is not REACTive! You need to put it into a useState for it to become REACTive!
-   // const handleClick = () => {
-   //    nonReactivename = "Ney Yaar";
-   //    console.log(nonReactivename);
-   // }
-
-   const [reactiveName, setReactiveName] = useState("Oi! Bhai Saab!");
-   const handleClick = () => {
-      setReactiveName("Ney Yaar");
-      console.log(reactiveName);
-   }
-   
-   // const handleClick = (ev) => {
-   //    console.log("Kidda Yaar?", ev);
-   // }
-   
-   // const handleClickAgain = (name, ev) => {
-   //    console.log("Kidda " + name, ev.target);
-   // }
-
-
+   const [blogs, setBlogs] = useState([
+      { title: "My new website", body: "Kidda Yaara?", author: "Bhai Saab", id: 1 },
+      { title: "Welcome...", body: "Kidda Yaara?", author: "Uncle Jee", id: 2 },
+      { title: "Some Top Tips", body: "Kidda Yaara?", author: "Mama", id: 3 }
+   ]);
 
    return (
       <div className="home">
-         <h2>Homepage</h2>
-
-         {/* <p>{ nonReactivename }</p> */}
-         <p>{ reactiveName }</p>
-
-         {/* If the included the brackets after the function name below, it would be invoked automatically - without even waiting for the user to click it. Therefore we only provide a reference to the function name */}
-         <button onClick={ handleClick }>Click It!</button>
-         
-         {/* <button onClick={ (event) => {
-            handleClickAgain('Yaara', event) // This is the way to pass arguments to a called function from here (and to prevent it being invoked automatically). "event" can be passed into this function as a the first OR the second argument
-         } }>Click It Again!</button> */}
-
+         { blogs.map((blog) => {
+            return (
+               <div className="blog-preview" key={ blog.id }>
+                  <h2>{ blog.title }</h2>
+                  <p>Written by { blog.author }</p>
+               </div>
+            )
+         })}
       </div>
    );
 }
