611889b5c9a89698bfc1851681b045cb9c9ee2d4
nahmed99 <ahmed_nawaz@hotmail.com>
2021-01-21 20:58:35 +0000

    created BlogList and the used props to pass data to it. Then commented out the props and used destructured data instead

---
+++ src/BlogList.js
@@ -0,0 +1,24 @@
+// using props:
+// const BlogList = (kiddas) => {
+//    const blogs = kiddas.blogs
+//    const title = kiddas.title
+
+// Destructuring, i.e., NOT using props - using {} instead for the destructured data
+const BlogList = ({ blogs, title }) => {
+
+   return ( 
+      <div className="blog-list">
+         <h2>{ title }</h2>
+         { blogs.map((blog) => {
+            return (
+               <div className="blog-preview" key={ blog.id }>
+                  <h2>{ blog.title }</h2>
+                  <p>Written by { blog.author }</p>
+               </div>
+            )
+         })}
+      </div>
+    );
+}
+ 
+export default BlogList;
