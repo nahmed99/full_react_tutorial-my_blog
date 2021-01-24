e0a39d1df14656b18f8890a6bd9f584260629f69
nahmed99 <ahmed_nawaz@hotmail.com>
2021-01-24 20:33:14 +0000

    useEffect added - renders each time there is a change of state

--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,11 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
 
 const Home = () => {
 
+   // useState controls the rendering of the page; the webpage renders 
+   // upon first being 'called', and then everytime there is a change of
+   // state (of a useStaet variable)...
    const [blogs, setBlogs] = useState([
       { title: "My new website", body: "Kidda Yaara?", author: "Bhai Saab", id: 1 },
       { title: "Welcome...", body: "Kidda Uncle?", author: "Uncle Jee", id: 2 },
@@ -15,6 +18,15 @@ const Home = () => {
       const newBlogs = blogs.filter(blog => blog.id !== id);
       setBlogs(newBlogs);
    }
+
+   // useEffect will run upon initial load (and render) and then anytime 
+   // there is a re-render upon state data changes.
+   // Be careful of any change of state within the useEffect - could end up
+   // with a continuous/indefinite loop of renders.
+   useEffect(() => {
+      console.log("useEffect RAN!!!");
+      console.log(blogs);
+   });
 
 
    return (
