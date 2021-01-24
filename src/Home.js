fc44cd43afb232101a11817e4a395dfeaf066ac2
nahmed99 <ahmed_nawaz@hotmail.com>
2021-01-24 20:44:28 +0000

    useEffect dependency added, so that it only runs when the state of the dependency changes (it will only run if the value of that state variable changes)

--- src/Home.js
+++ src/Home.js
@@ -13,6 +13,8 @@ const Home = () => {
       { title: "Some othee website", body: "Kidda Bhai?", author: "Bhai Saab", id: 4 }
    ]);
 
+   const [name, setName] = useState('Kidda?');
+
 
    const handleDelete = (id) => {
       const newBlogs = blogs.filter(blog => blog.id !== id);
@@ -23,16 +25,26 @@ const Home = () => {
    // there is a re-render upon state data changes.
    // Be careful of any change of state within the useEffect - could end up
    // with a continuous/indefinite loop of renders.
+   // useEffect(() => {
+   //    console.log("useEffect RAN!!!");
+   //    console.log(blogs);
+   // }, []); 
+   // useEffect dependency array - to control when it runs, after re-render.
+   // An empty array ensures that useEffect only runs after the initial render.
+   // It doesn't run on any subsequent renders.
+
    useEffect(() => {
       console.log("useEffect RAN!!!");
-      console.log(blogs);
-   });
+      console.log(name);
+   }, [name]);  // This useEffect will only run when the state (name) changes.
 
 
    return (
       <div className="home">
          <BlogList blogs={ blogs } title="My NEW Blog" handleDelete={handleDelete} 
          />
+         <button onClick={() => setName("Yaara")}>Change Name</button>
+         <p>{ name }</p>
       </div>
    );
 }
