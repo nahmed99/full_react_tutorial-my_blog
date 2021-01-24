17749f822b17e53522e3404b6868ea4f88571137
nahmed99 <ahmed_nawaz@hotmail.com>
2021-01-24 20:13:46 +0000

    re-using components

--- src/Home.js
+++ src/Home.js
@@ -5,13 +5,15 @@ const Home = () => {
 
    const [blogs, setBlogs] = useState([
       { title: "My new website", body: "Kidda Yaara?", author: "Bhai Saab", id: 1 },
-      { title: "Welcome...", body: "Kidda Yaara?", author: "Uncle Jee", id: 2 },
-      { title: "Some Top Tips", body: "Kidda Yaara?", author: "Mama", id: 3 }
+      { title: "Welcome...", body: "Kidda Uncle?", author: "Uncle Jee", id: 2 },
+      { title: "Some Top Tips", body: "Kidda Bey?", author: "Mama", id: 3 },
+      { title: "Some othee website", body: "Kidda Bhai?", author: "Bhai Saab", id: 4 }
    ]);
 
    return (
       <div className="home">
          <BlogList blogs={ blogs } title="My NEW Blog" />
+         <BlogList blogs={ blogs.filter((blog) => blog.author === "Bhai Saab") } title="Bhai Saab's Blogs" />
       </div>
    );
 }
