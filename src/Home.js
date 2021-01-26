64c8a4f505ffc7cc29b8eeb56ee4a6ab40a397d8
nahmed99 <ahmed_nawaz@hotmail.com>
2021-01-26 21:52:55 +0000

    add some error handling code, for netwwork error and for incorrect URL in home.js, for the fetch

--- src/Home.js
+++ src/Home.js
@@ -3,12 +3,16 @@ import BlogList from './BlogList';
 
 const Home = () => {
 
+   const URL = 'http://localhost:8000/blogs';
+
    // useState controls the rendering of the page; the webpage renders 
    // upon first being 'called', and then everytime there is a change of
    // state (of a useStaet variable)...
    const [blogs, setBlogs] = useState(null);
 
    const [isPending, setIsPending] = useState(true);
+
+   const [error, setError] = useState(null);
 
    const [name, setName] = useState('Kidda?');
 
@@ -18,12 +22,28 @@ const Home = () => {
    // Be careful of any change of state within the useEffect - could end up
    // with a continuous/indefinite loop of renders.
    useEffect(() => {
-      fetch('http://localhost:8000/blogs')
+      fetch(URL)
       .then(res => {
+         console.log(res);
+         if (!res.ok) {
+            // this checks if the request was unsuccessful - e.g., it won't be if our URL was incorrect - server fine, url etc not okay.
+
+            // This throw Error will be caught in the catch statement below.
+            throw Error('Could not fetch the data from requested URL: ' + URL);
+
+         }
          return res.json()
       })
       .then((data) => {
          setBlogs(data); // This won't cause an indefinite loop, due to the empty dependency array below.
+         setIsPending(false);
+         setError(null);
+      })
+      .catch(err => {
+         // This catch is for cases where we can't connect to the server, OR
+         // for errors that we have thrown above.
+         // console.log("FETCH ERROR: " + err.message);
+         setError("FETCH ERROR: " + err.message);
          setIsPending(false);
       })
    }, []); 
@@ -39,6 +59,8 @@ const Home = () => {
 
    return (
       <div className="home">
+
+         { error && <h3> { error }</h3> }
 
          {isPending && <div><h1>Loading...</h1></div>}
 
