849ba8c0b56d8d6baed5edbf20030e5180ed7d2e
nahmed99 <ahmed_nawaz@hotmail.com>
2021-01-28 22:08:25 +0000

    Created a second URL for React ROUTE and also added the *exact* parameter to ensure that selected url is an exact match

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Navbar from './Navbar';
 import Home from './Home';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Create from './Create';
 
 function App() {
   
@@ -10,10 +11,17 @@ function App() {
         <Navbar />
         <div className="content">
           <Switch>
-            {/* This makes sure that only one route shows at any one time */}
-            <Route path="/">
+            {/* This switch makes sure that only one route shows in browser at any one time. It checks each route, from top to bottom and stops at the first match found */}
+
+            {/* The 'exact' paramater/prop ensures that the route is selected only when it matches exactly with the url that the user uses */}
+            <Route exact path="/">
               <Home />
             </Route>
+
+            <Route path="/create">
+              <Create />
+            </Route>
+
           </Switch>
         </div>
       </div>
